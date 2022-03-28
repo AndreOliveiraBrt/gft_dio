1459d4b1248ab59d8e81a8b15497b17bd9e682e4
AndreOliveiraBrt <93344694+AndreOliveiraBrt@users.noreply.github.com>
2022-03-27 21:34:56 -0300

    teste

--- src/exercicios_java/Ex1_OrdemInversa.java
+++ src/exercicios_java/Ex1_OrdemInversa.java
@@ -17,6 +17,9 @@ public class Ex1_OrdemInversa {
 	        for(int i = (vetor.length - 1); i >= 0; i --) {
 	            System.out.print(vetor[i] + " ");
 	        }
+	        
+	        
+	        
 
 	}
 
