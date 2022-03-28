f944139d3515ef0fb97d50c78bb8973c1ba84423
AndreOliveiraBrt <93344694+AndreOliveiraBrt@users.noreply.github.com>
2022-03-27 21:52:07 -0300

    organização do codigo

--- src/exercicios_java/Ex1_OrdemInversa.java
+++ src/exercicios_java/Ex1_OrdemInversa.java
@@ -1,6 +1,11 @@
 package exercicios_java;
 
 public class Ex1_OrdemInversa {
+	
+	/*
+	Crie um vetor de 6 números inteiros
+	e mostre-os na ordem inversa.
+	*/
 
 	public static void main(String[] args) {
 		
@@ -17,10 +22,7 @@ public class Ex1_OrdemInversa {
 	        for(int i = (vetor.length - 1); i >= 0; i --) {
 	            System.out.print(vetor[i] + " ");
 	        }
-	        
-	        
-	        
-
+                     
 	}
 
 }
